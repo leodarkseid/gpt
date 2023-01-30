cbf04463884839d16965a773aa820822bbfd0d74
kolade Badmus <leodarkseid@gmail.com>
2023-01-30 15:33:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 
-import {Article, Brand, CTA } from './components';
+import {Footer, Blog, Possibility, Features, Gpt3,Header } from './containers';
+import {Navabar, Brand, CTA } from './components';
 const App = () => {
   return (
-    <div>
-        <h1>GPT</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+
+
+
+      </div>
+      <Brand />
+      <Gpt3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+       
     </div>
   )
 }
