02e55a4835d3f2bcd12e34f3fc6cc74464295f61
kolade Badmus <leodarkseid@gmail.com>
2023-01-30 16:45:03 +0100

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 
 const Navbar = () => {
   return (
-    <div className='gpt3__navabar'>
+    <div className='gpt3__navbar'>
       <div className="gpt3__navbar-links">
         <div className="gpt3_navbar-links_logo">
           <img src={logo} alt="logo" />
