ef168c4a6523a4beb3d4d160c11b28feb57083cb
kolade Badmus <leodarkseid@gmail.com>
2023-01-30 19:22:55 +0100

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,9 +8,20 @@ const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className="gpt3__navbar-links">
-        <div className="gpt3_navbar-links_logo">
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT ?</a></p>
+          <p><a href='#possibiltiy'></a>Open Ai</p>
+          <p><a href='#features'></a>Case Studies</p>
+          <p><a href='#blog'></a>Library</p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type='button'>sign up</button>
       </div>
     </div>
     
