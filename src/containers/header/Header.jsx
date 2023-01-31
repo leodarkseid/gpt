776c0bc22ec075e330ad23e0adf5b08f2f027ba8
kolade Badmus <leodarkseid@gmail.com>
2023-01-31 23:33:44 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
@@ -12,6 +14,16 @@ const Header = () => {
         <input type="email" placeholder/>
         <button type='button'>Get Started</button>
       </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} alt='people' />
+        <p>1,600 people requested access in the last 24hours </p>
+      </div>
+
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+
       </div>
       </div>
   )
