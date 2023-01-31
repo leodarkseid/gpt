25fcc122ba1eb273fbfd95c35d01c5b6f710cce1
kolade Badmus <leodarkseid@gmail.com>
2023-01-31 23:19:41 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,17 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section_padding' id='home'>
+      <div className="gpt3__header-content">
+        <h1 className='gradient__text'>Let's Build something amazing with GPT-3 OpenAI </h1>
+      <p>Sit non amet voluptate culpa exercitation elit et proident aliquip. Reprehenderit excepteur incididunt ipsum dolor culpa nulla aliquip sunt cupidatat. Aliquip qui tempor officia amet .</p>
+
+      <div className="gpt3__header-content-input">
+        <input type="email" placeholder/>
+        <button type='button'>Get Started</button>
+      </div>
+      </div>
+      </div>
   )
 }
 
