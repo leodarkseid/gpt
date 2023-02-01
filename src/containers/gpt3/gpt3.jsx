033eb95e482fcd13bb983e4f91c07060671dd8e4
kolade Badmus <leodarkseid@gmail.com>
2023-02-01 16:01:10 +0100

    update

--- src/containers/gpt3/gpt3.jsx
+++ src/containers/gpt3/gpt3.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components';
+
 import './gpt3.css';
 
 const Gpt3 = () => {
   return (
-    <div>Gpt3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whatgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__gpt3-heading">
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgp3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+        
+      </div>
+
+    </div>
   )
 }
 
