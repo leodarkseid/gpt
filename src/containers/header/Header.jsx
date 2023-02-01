4f9a4b72929573b98f95d7785403a51e907d47aa
kolade Badmus <leodarkseid@gmail.com>
2023-02-01 14:41:23 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div className='gpt3__header section_padding' id='home'>
+    <div className='gpt3__header section__padding' id='home'>
       <div className="gpt3__header-content">
         <h1 className='gradient__text'>Let's Build something amazing with GPT-3 OpenAI </h1>
       <p>Sit non amet voluptate culpa exercitation elit et proident aliquip. Reprehenderit excepteur incididunt ipsum dolor culpa nulla aliquip sunt cupidatat. Aliquip qui tempor officia amet .</p>
@@ -20,11 +20,12 @@ const Header = () => {
         <p>1,600 people requested access in the last 24hours </p>
       </div>
 
-      {/* <div className="gpt3__header-image">
-        <img src={ai} alt="ai" />
-      </div> */}
-
       </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+
+      
       </div>
   )
 }
