a0d3580d46a32783e9fa8020d0bc734ddbdfc630
kolade Badmus <leodarkseid@gmail.com>
2023-02-03 22:44:53 +0100

    update

--- src/containers/gpt3/gpt3.jsx
+++ src/containers/gpt3/gpt3.jsx
@@ -7,7 +7,7 @@ const Gpt3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='whatgpt3'>
       <div className="gpt3__whatgpt3-feature">
-        <Feature title="What is GPT-3"/>
+        <Feature title="What is GPT-3 "/>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
