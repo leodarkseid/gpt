21b6309ef0b982b621fdeba267a07350f303ada9
kolade Badmus <leodarkseid@gmail.com>
2023-02-06 21:46:48 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className="gpt3__header-content">
         <h1 className='gradient__text'>Let's Build something amazing with GPT-3 OpenAI </h1>
-      <p>Sit non amet voluptate culpa exercitation elit et proident aliquip.Reprehenderit excepteur incididunt ipsum dolor culpa nulla aliquip sunt cupidatat. Aliquip qui tempor officia amet .</p>
+      <p>Sit non amet voluptate culpa exercitation elit et proident aliquip.Reprehenderit excepteur incididunt ipsum dolor culpa nulla aliquip sunt cupidatat.Aliquip qui tempor officia amet .</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address"/>
